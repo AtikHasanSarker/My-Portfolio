8fd6c766faa3dc5a1e0c3285b98ef3f9a40e41a0
AtikHasanSarker <atiksagor24@gmail.com>
2026-08-21 04:19:35 +0600

    Fixed widget issues

--- app/layout.jsx
+++ app/layout.jsx
@@ -33,11 +33,19 @@ export default function RootLayout({ children }) {
           </div>
         </Providers>
         <BackToTop />
-        <Script
-          id="tawk-to"
-          src="https://embed.tawk.to/6a8772e1b56df5344af1bc05/1k0ghgh07"
-          strategy="afterInteractive"
-        />
+        <Script id="tawk-to-widget" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function(){
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = 'https://embed.tawk.to/6a8772e1b56df5344af1bc05/1k0ghhgo7';
+              s1.charset = 'UTF-8';
+              s1.setAttribute('crossorigin', '*');
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
